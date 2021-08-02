2c7819ab15456e075728a748de1b185e84d3172a
Aman sharma <amansharma1823756@gmail.com>
2021-08-03 00:32:15 +0530

    Music player first commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, {useState, useRef} from 'react'
+//import Styles
+import './styles/app.scss';
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+//Import util
+import data from "./data"
+
+function App() {
+  //Ref
+  const audioRef = useRef(null);
+
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+});
+
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  //Handler
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    const animation = Math.round((Math.round(current)/Math.round(duration))*100);
+    setSongInfo({...songInfo, currentTime: current, duration: duration, animationPercentage: animation})
+    console.log(e.target.currentTime);
+}
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((song)=>song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1)%songs.length]);
+    if(isPlaying){
+      audioRef.current.play();
+    }
+  }
+  return (
+    <div className={`App ${libraryStatus ? "library-active": ""}`}>
+        <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}></Nav>
+        <Song currentSong={currentSong}/>
+        <Player 
+          audioRef={audioRef}
+          isPlaying={isPlaying}
+          setIsPlaying={setIsPlaying}
+          currentSong={currentSong}
+          setSongInfo={setSongInfo}
+          songInfo={songInfo}
+          songs={songs}
+          setCurrentSong={setCurrentSong}
+          setSongs = {setSongs}
+        />
+        <Library 
+          audioRef={audioRef}
+          songs={songs} 
+          setCurrentSong={setCurrentSong}
+          isPlaying={isPlaying}
+          setSongs={setSongs}
+          libraryStatus={libraryStatus}
+        />
+        <audio 
+          onTimeUpdate={timeUpdateHandler} 
+          onLoadedMetadata={timeUpdateHandler} 
+          ref={audioRef} 
+          src={currentSong.audio}
+          onEnded={songEndHandler}
+        ></audio>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,100 @@
+import {v4 as uuidv4} from "uuid";
+
+function chillHop() {
+    return [
+        {
+            name: "Higher",
+            artist: "Misha, Jussi Halme",
+            cover: "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+            id: uuidv4(),
+            active: true,
+            color: ["#AC99AD", "#EB7876"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=15041"
+
+        },
+        {
+            name: "Travelbag",
+            artist: "Mommy, Sleepy Fish",
+            cover: "https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#5E91D2", "#B7BFC1"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=19058"
+        },
+        {
+            name: "Viatecture",
+            artist: "Leavv, Makzo",
+            cover: "https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#EDC5B3", "#EECDB6"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=20119"
+        },
+        {
+            name: "New Moon",
+            artis: "Toonorth",
+            cover: "https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#020204", "#FEE368"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=17931"
+        },
+        {
+            name: "Dawn",
+            artist: "Toonorth",
+            cover: "https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#3D5172", "#F9F9F9"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=20125"
+        },
+        {
+            name: "Higher",
+            artist: "Misha, Jussi Halme",
+            cover: "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+            id: uuidv4(),
+            active: true,
+            color: ["#AC99AD", "#EB7876"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=15041"
+
+        },
+        {
+            name: "Travelbag",
+            artist: "Mommy, Sleepy Fish",
+            cover: "https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#5E91D2", "#B7BFC1"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=19058"
+        },
+        {
+            name: "Viatecture",
+            artist: "Leavv, Makzo",
+            cover: "https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#EDC5B3", "#EECDB6"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=20119"
+        },
+        {
+            name: "New Moon",
+            artis: "Toonorth",
+            cover: "https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#020204", "#FEE368"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=17931"
+        },
+        {
+            name: "Dawn",
+            artist: "Toonorth",
+            cover: "https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#3D5172", "#F9F9F9"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=20125"
+        }
+    ]
+}
+
+export default chillHop;
